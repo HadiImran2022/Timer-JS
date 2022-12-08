fa85b9f495d91f5c0067ce9de4d1db73e67f4aac
HadiImran2022 <abdulhadiimran2022@gmail.com>
2022-12-08 15:00:50 +0500

    Timer Complete JS

---
+++ app.js
@@ -0,0 +1,23 @@
+var endDate = prompt("08 December 03:20:00 PM");
+
+document.getElementById("end-date").innerText = endDate;
+var inputs = document.querySelectorAll("input");
+
+function clock() {
+  var end = new Date(endDate);
+  var now = new Date();
+  var diff = (end - now) / 1000;
+
+  if (diff < 0) return;
+
+  inputs[0].value = Math.floor(diff / 3600 / 24);
+  inputs[1].value = Math.floor(diff / 3600) % 24;
+  inputs[2].value = Math.floor(diff / 60) % 60;
+  inputs[3].value = Math.floor(diff) % 60;
+}
+
+clock();
+
+setInterval(() => {
+  clock();
+}, 1000);
